f2324148001a7c9abfae2341b8a6a91889d3c85d
Eduardo Zacchi <edzacchi@gmail.com>
2025-08-18 14:43:22 -0300

    fix(Movie): fix an issue where POST request  returned the names of a movies categories as null

--- src/main/java/com/movievault/service/MovieService.java
+++ src/main/java/com/movievault/service/MovieService.java
@@ -1,10 +1,13 @@
 package com.movievault.service;
 
+import com.movievault.entity.Category;
 import com.movievault.entity.Movie;
+import com.movievault.entity.Streaming;
 import com.movievault.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +16,8 @@ import java.util.Optional;
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public List<Movie> getAllMovies(){
         return movieRepository.findAll();
@@ -23,10 +28,24 @@ public class MovieService {
     }
 
     public Movie saveMovie(Movie movie){
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreaming(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public void deleteMovie(Long id){
         movieRepository.deleteById(id);
+    }
+
+    public List<Category> findCategories(List<Category> categories){
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(c -> categoryService.getCategoryById(c.getId()).ifPresent(categoriesFound::add));
+        return categoriesFound;
+    }
+
+    public List<Streaming> findStreaming(List<Streaming> streamings){
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(c -> streamingService.getStreamingById(c.getId()).ifPresent(streamingsFound::add));
+        return streamingsFound;
     }
 }
