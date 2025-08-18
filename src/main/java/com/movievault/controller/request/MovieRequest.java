b10161d07d73b18be7b1a09eaa1323c6d040a346
Eduardo Zacchi <edzacchi@gmail.com>
2025-08-18 18:34:28 -0300

    style(Movie): change MovieRequest date format to dd/MM/yyyy

--- src/main/java/com/movievault/controller/request/MovieRequest.java
+++ src/main/java/com/movievault/controller/request/MovieRequest.java
@@ -1,11 +1,16 @@
 package com.movievault.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record MovieRequest(String title,
                            String description,
+
+                           @JsonFormat(pattern = "dd/MM/yyyy")
                            LocalDate releaseDate,
+
                            double rating,
                            List<Long> categories,
                            List<Long> streamings
