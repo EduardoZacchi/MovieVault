e0cf04f178ee96310935f03686ba46648d0fca57
Eduardo Zacchi <edzacchi@gmail.com>
2025-08-21 13:36:09 -0300

    feat(User): add user entity and database migration

---
+++ src/main/java/com/movievault/entity/User.java
@@ -0,0 +1,27 @@
+package com.movievault.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
